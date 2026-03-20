0b97618f8e2cece552ac088185c0d64bede6364d
Victor Landim <victorlandim5@gmail.com>
2026-03-20 10:00:12 +0100

    add update

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     return (
         <main>
             <h1>Next.js Counter</h1>
+            <p>This is an update.</p>
             <Counter />
         </main>
     )
