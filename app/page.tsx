54b92aa2c738c823cc78e9e6b38869c8b47dd675
Victor Landim <victorlandim5@gmail.com>
2026-03-20 12:03:15 +0100

    fix code

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,9 @@ import Counter from "./components/Counter"
 import "./globals.css"
 
 export default function Home() {
-    var content = '';
     return (
         <main>
-
-
-
-
-
-
-
             <h1>Next.js Counter</h1>
-
-
-
-
 
             <p>This is an update.</p>
             <p>This is another update.</p>
